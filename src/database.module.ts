4a694ac0b8c135bd60f6ef71ff56de0e4deae3dd
almalkias <almalkias@icloud.com>
2026-08-31 20:51:32 +0300

    Initial NestJS setup with PostgreSQL and Firebase Admin

---
+++ src/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        entities: [],
+        synchronize: false,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
