de62c771725e1b9a4c3d15686645f6cb495127d0
almalkias <almalkias@icloud.com>
2026-09-07 10:56:56 +0300

    Enable CORS for frontend origin

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: 'http://localhost:5173' });
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
