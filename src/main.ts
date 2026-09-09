a10cde1587779bee96c274a5a3f4c811c2689ac3
almalkias <almalkias@icloud.com>
2026-09-09 19:08:49 +0300

    allow Netlify production origin in CORS config

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: 'http://localhost:5173' });
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://tickets-flow.netlify.app',
+    ],
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
